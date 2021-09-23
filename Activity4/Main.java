d103a3e73cc1232474943b9b53d2c7d03db09598
Caden <35741667+cadentj@users.noreply.github.com>
2021-09-23 12:17:01 -0500

    syntax fixes

--- Activity4/Main.java
+++ Activity4/Main.java
@@ -2,8 +2,8 @@ class Main {
   public static void main(String args[]) {
     System.out.prinln("Caden");
     System.out.prinln("Juang");
-    System.out.println("cjuang23@sjs.org")
-    System.out.println("This is PDS!!")
+    System.out.println("cjuang23@sjs.org");
+    System.out.println("This is PDS!!");
     System.out.println("Fall 2021");
   }
 }
