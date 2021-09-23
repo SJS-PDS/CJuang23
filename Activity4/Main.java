b4cbb96af6e19a78784feb99431dc9d5447f56b6
Caden <35741667+cadentj@users.noreply.github.com>
2021-09-23 10:56:06 -0500

    Edited Main.java again

--- Activity4/Main.java
+++ Activity4/Main.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String args[]) {
     System.out.prinln("Caden");
     System.out.prinln("Juang");
-    System.out.println("cjuang23@sjs.org")
+    System.out.println("cjuang23@sjs.org");
+    System.out.println("Fall 2021");
   }
 }
